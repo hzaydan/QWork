e704be9f49f249dce1bdfcebf8f99327f089fdac
hzaydan <hzaydan@dtp.ae>
2021-02-28 21:21:38 +0200

    work in progress

--- qwork-kernel/src/main/java/org/qwork/kernel/db/Qdb.java
+++ qwork-kernel/src/main/java/org/qwork/kernel/db/Qdb.java
@@ -1,6 +1,15 @@
 package org.qwork.kernel.db;
 
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import org.qwork.kernel.db.jaxb.HolyQuran;
+
 public class Qdb {
+	
+	private static HolyQuran CannotBeLoaded = null;
 	
 	private QChapter[] chapters;
 
@@ -12,4 +21,16 @@ public class Qdb {
 		this.chapters = chapters;
 	}
 
+	public HolyQuran load() {
+		try {
+			Unmarshaller un = JAXBContext.newInstance(HolyQuran.class).createUnmarshaller();
+			InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("QuranDb/HolyQuran.xml");
+			HolyQuran qdb = (HolyQuran) un.unmarshal(in);
+			return qdb;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return CannotBeLoaded;
+		}
+	}
 }
--- qwork-kernel/src/test/java/org/qwork/kernel/test/TestServices.java
+++ qwork-kernel/src/test/java/org/qwork/kernel/test/TestServices.java
@@ -1,9 +1,14 @@
 package org.qwork.kernel.test;
 
+import org.qwork.kernel.db.Qdb;
+import org.qwork.kernel.db.jaxb.HolyQuran;
+
 public class TestServices {
 	
 	public static void main(String[] args) {
-		
+		Qdb qdb = new Qdb();
+		HolyQuran Q = qdb.load();
+		System.out.println(Q.getSurah().get(0).getAyah().get(0).getValue());
 	}
 
 }
